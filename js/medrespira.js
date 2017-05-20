a279eacfd49de9f0b25d76da5ed62802908bcb59
AlejandroJSR7 <AlejandroJSR7@Gmail.com>
2017-05-20 00:05:27 -0500

    New Design

---
+++ js/medrespira.js
@@ -0,0 +1,11 @@
+console.info('JS_File', 'medrespira.js');
+
+$.backstretch([
+      "images/content/hero/hero_001.jpg"
+    , "images/content/hero/hero_002.jpg"
+    , "images/content/hero/hero_003.jpg"
+    , "images/content/hero/hero_004.jpg"
+    , "images/content/hero/hero_005.jpg"
+    , "images/content/hero/hero_006.jpg"
+    , "images/content/hero/hero_007.jpg"
+  ], {duration: 4000, fade: 2000});
